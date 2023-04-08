c80445e9df95e008760a5cb9e524b432b49d8efa
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-08 16:58:18 -0500

    Add form validation for name and text area

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,6 +1,9 @@
-const formInput = document.querySelector('form input[type="email"]');
+const formInputNodes = document.querySelectorAll('form input');
 const formSumitValidation = document.getElementById('contact-form');
 const formMessage = document.getElementById('validation');
+const formTextArea = document.querySelector('form textarea');
+const formInputs = Array.from(formInputNodes);
+formInputs.push(formTextArea);
 
 const patterns = {
   email: /^(([^<>()[\]\\/.,;:\s*@"]+(\.[^<>()[\]\\/.,;:\s*@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
@@ -15,19 +18,43 @@ function validate(field, regex) {
     field.classList.add('invalid');
   }
 }
-
-function lookInvalid() {
-  return formInput.classList.contains('invalid');
+function validateSize(field, size) {
+  if (field.value.length <= size && field.value.length > 0) {
+    field.classList.remove('invalid');
+    field.classList.add('valid');
+  } else {
+    field.classList.remove('valid');
+    field.classList.add('invalid');
+  }
 }
 
-formInput.addEventListener('keyup', (e) => {
-  validate(e.target, patterns[e.target.attributes.name.value]);
+function lookInvalid() {
+  formInputs.forEach((formInput) => formInput.classList.contains('invalid'));
+}
+
+formInputs.forEach((formInput) => {
+  formInput.addEventListener('keyup', (e) => {
+    const input = e.target.attributes;
+    switch (input.name.value) {
+      case 'name':
+        validateSize(e.target, 30);
+        break;
+      case 'email':
+        validate(e.target, patterns[input.name.value]);
+        break;
+      case 'contact-descrip':
+        validateSize(e.target, 500);
+        break;
+      default:
+        break;
+    }
+  });
 });
 
 formSumitValidation.addEventListener('submit', (e) => {
   formMessage.style.fontSize = '18px';
   formMessage.style.fontWeight = 'bold';
-  if (lookInvalid()) {
+  if (lookInvalid) {
     e.preventDefault();
     formMessage.style.color = '#f00';
     formMessage.textContent = 'You need to fill correctly the form!';
