97f69fbde50fcc7e2c7a63a264020a1e1110e7b8
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-10 05:11:02 -0500

    add menu modal resize functionality

--- scripts/menuModal.js
+++ scripts/menuModal.js
@@ -4,24 +4,26 @@ const menuPopUp = document.querySelector('.menu');
 const body = document.querySelector('body');
 
 function menuModal() {
-  console.log(menuPopUp);
+  menuHamburger.computedStyleMap.display = 'none';
   body.classList.add('modal-active');
   menuPopUp.classList.add('menu_popup');
 }
 
 function closeMenuModal() {
-  console.log(menuPopUp);
   menuPopUp.classList.add('out');
+  menuHamburger.computedStyleMap.display = 'block';
   body.classList.remove('modal-active');
   setTimeout(() => menuPopUp.classList.remove('menu_popup', 'out'), 500);
 }
 
-menuHamburger.addEventListener('click', menuModal);
-closeMenu.forEach(element => {
-  element.addEventListener('click', closeMenuModal);
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 768) {
+    menuPopUp.classList.remove('menu_popup', 'out');
+    body.classList.remove('modal-active');
+  }
 });
 
-
-
-
-
+menuHamburger.addEventListener('click', menuModal);
+closeMenu.forEach((element) => {
+  element.addEventListener('click', closeMenuModal);
+});
