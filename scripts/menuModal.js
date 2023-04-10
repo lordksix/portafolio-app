df9904fa47a26fac7c3d86952060bd18e1cb3207
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-04-10 14:11:35 +0500

    Add close-menu functionality

--- scripts/menuModal.js
+++ scripts/menuModal.js
@@ -1,9 +1,27 @@
 const menuHamburger = document.querySelector('.menu_hamburger');
+const closeMenu = document.querySelectorAll('.menu li');
 const menuPopUp = document.querySelector('.menu');
+const body = document.querySelector('body');
 
 function menuModal() {
   console.log(menuPopUp);
+  body.classList.add('modal-active');
   menuPopUp.classList.add('menu_popup');
 }
 
+function closeMenuModal() {
+  console.log(menuPopUp);
+  menuPopUp.classList.add('out');
+  body.classList.remove('modal-active');
+  setTimeout(() => menuPopUp.classList.remove('menu_popup', 'out'), 500);
+}
+
 menuHamburger.addEventListener('click', menuModal);
+closeMenu.forEach(element => {
+  element.addEventListener('click', closeMenuModal);
+});
+
+
+
+
+
