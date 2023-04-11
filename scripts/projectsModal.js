f1a478ae5ed63469faa35f4b27683228eec3ea7f
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-11 03:27:26 -0500

    create object for dynamic work overlay render

---
+++ scripts/projectsModal.js
@@ -0,0 +1,50 @@
+const works = [
+  {
+    name: 'Tonic',
+    cat: ['CANOPY', 'Back End Dev', 2015],
+    description:
+      `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    snapshot: './images/Snapshoot-Portfolio.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: ['See live', 'See source'],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    cat: ['FACEBOOK', 'Full Stack Dev', 2015],
+    description:
+    `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    snapshot: './images/Snapshoot-multi.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: ['See live', 'See source'],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+  {
+    name: 'Facebook 360',
+    cat: ['FACEBOOk', 'Full Stack Dev', 2015],
+    description:
+    `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    snapshot: './images/topic-porta.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: ['See live', 'See source'],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+  {
+    name: 'Uber Navigation',
+    cat: ['Uber', 'Lead Developer', 2018],
+    description:
+    `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.`,
+    tags: ['HTML', 'CSS', 'Ruby on Rails', 'JavaScript'],
+    snapshot: './images/mul-stories-porta.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: ['See live', 'See source'],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+];
