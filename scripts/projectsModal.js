5edf714a36e065b75fcdf3f871f635a240ca80af
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-12 05:38:00 -0500

    Change rendering form of works section

--- scripts/projectsModal.js
+++ scripts/projectsModal.js
@@ -52,6 +52,53 @@ const works = [
   },
 ];
 
+const works2 = [
+  {
+    name: 'Tonic',
+    cat: ['CANOPY', 'Back End Dev', 2015],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    snapshot: './images/Snapshoot-Portfolio.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: [btnLive, btnSource],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+  {
+    name: 'Multi-Post Stories',
+    cat: ['FACEBOOK', 'Full Stack Dev', 2015],
+    description:
+    'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    snapshot: './images/Snapshoot-multi.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: [btnLive, btnSource],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+  {
+    name: 'Facebook 360',
+    cat: ['FACEBOOk', 'Full Stack Dev', 2015],
+    description:
+    'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    snapshot: './images/topic-porta.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: [btnLive, btnSource],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+  {
+    name: 'Uber Navigation',
+    cat: ['Uber', 'Lead Developer', 2018],
+    description:
+    'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    tags: ['HTML', 'CSS', 'Ruby on Rails', 'JavaScript'],
+    snapshot: './images/mul-stories-porta.svg',
+    connectionsBtn: ['./images/source-live-btn.svg', './images/source-github-btn.svg'],
+    connectionstext: [btnLive, btnSource],
+    connectionsURL: ['https://lordksix.github.io/portafolio-app/', 'https://github.com/lordksix/portafolio-app'],
+  },
+];
+
 const projectBtns = document.querySelectorAll('.work .work-btn');
 const bodyTag = document.querySelector('body');
 
@@ -153,4 +200,80 @@ function projectModal(e) {
   closeBtn.addEventListener('click', () => bodyTag.removeChild(projectOverlay));
 }
 
+function renderworks() {
+  const docFragmentSd = document.createDocumentFragment();
+  const docFragment = document.createDocumentFragment();
+  const docFragementFinal = document.createDocumentFragment();
+  
+  const projectOverlay = document.createElement('ul');
+  projectOverlay.classList.add('works');
+  const workSection = document.querySelector('#works');
+
+  for(let i = 0; i < works2.length; i += 1) {
+    const workCardInf = works2[i];
+    const projectCtn = document.createElement('li');
+    projectCtn.classList.add('work');
+
+    const workImage = document.createElement('img');
+    workImage.classList.add('work-image');
+    workImage.src = workCardInf.snapshot;
+    workImage.alt = `${workCardInf.name} Project, ${workCardInf.cat[2]}`;
+    docFragmentSd.appendChild(workImage);
+
+    const workCard = document.createElement('div');
+    workCard.classList.add('work-card');
+
+    const projectHeading = document.createElement('h2');
+    projectHeading.textContent = workCardInf.name;
+    projectHeading.classList.add('work-title');
+    docFragment.appendChild(projectHeading);
+
+    const workCat = document.createElement('ul');
+    workCat.classList.add('work-cat');
+    for (let i = 0; i < workCardInf.cat.length; i += 1) {
+      const catList = document.createElement('li');
+      if (i === 0) catList.classList.add('cat-title');
+      else catList.classList.add('cat-descrip');
+      catList.textContent = workCardInf.cat[i];
+      workCat.appendChild(catList);
+    }
+    docFragment.appendChild(workCat);
+
+    const workDescrip = document.createElement('p');
+    workDescrip.classList.add('work-descrip');
+    workDescrip.textContent = workCardInf.description;
+    docFragment.appendChild(workDescrip);
+
+    const workCardLangs = document.createElement('ul');
+    workCardLangs.classList.add('work-langs');
+    workCardInf.tags.forEach((tag) => {
+      const langList = document.createElement('li');
+      langList.classList.add('work-lang');
+      langList.textContent = tag;
+      workCardLangs.appendChild(langList);
+    });
+    docFragment.appendChild(workCardLangs);
+
+    const workCardBtnsCtn = document.createElement('div');
+    workCardBtnsCtn.classList.add('work-btns');
+    const workCardBtns = document.createElement('button');
+    workCardBtns.classList.add('work-btn');
+    const workCardBtnsText = document.createElement('span');
+    workCardBtnsText.textContent = 'See project';
+    workCardBtns.appendChild(workCardBtnsText);
+    workCardBtnsCtn.append(workCardBtns);
+  docFragment.appendChild(workCardBtnsCtn);
+    workCard.append(docFragment);
+    docFragmentSd.appendChild(workCard);
+    projectCtn.appendChild(docFragmentSd);
+    docFragementFinal.appendChild(projectCtn);
+  }
+  
+  projectOverlay.appendChild(docFragementFinal);
+  workSection.appendChild(projectOverlay);
+}
+
+
+window.addEventListener('load', renderworks);
 projectBtns.forEach((btn) => btn.addEventListener('click', projectModal));
+
