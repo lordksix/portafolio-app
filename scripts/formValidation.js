1526f7f804e44ea5ba52bcea817ab97efbba0bde
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-04-13 13:48:32 +0500

    Add an event listener for form validation

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -2,8 +2,13 @@ const formInputNodes = document.querySelector('form input[type="email"]');
 const sumbitBtn = document.querySelector('form button[type="submit"]');
 const errorMessage = document.getElementById('validation');
 
-function checkLowercase(input) {
-  if (input !== input.toLowerCase()) {
+function checkLowercase(e) {
+  if (formInputNodes.value !== formInputNodes.value.toLowerCase()) {
+    e.preventDefault();
     errorMessage.textContent = 'Form was not submitted. Email should be lower-cased.';
+  } else {
+    errorMessage.textContent = '';
   }
 }
+
+sumbitBtn.addEventListener('click', checkLowercase);
