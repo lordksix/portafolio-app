06bdd17130a0b841c3de05fe6890ff4e07655e5b
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-13 17:51:37 -0500

    Add load local storage functionality and refactor save to local storage

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -4,27 +4,24 @@ const errorMessage = document.getElementById('validation');
 
 const formInputs = document.querySelectorAll('form input');
 const formTextArea = document.querySelector('form textarea');
-const resetBtn = document.querySelector('form button[type="submit"]');
+const resetBtn = document.querySelector('form button[type="reset"]');
 const formInputsArr = Array.from(formInputs);
 formInputsArr.push(formTextArea);
 const formStorage = {
   name: '',
   email: '',
   message: '',
-}
+};
 
-function loadLocalStorage(){
-
+function loadLocalStorage() {
+  const formStr = localStorage.getItem('formData');
+  const formData = JSON.parse(formStr);
+  formInputsArr.forEach((formInput) => {
+    formInput.value = formData[formInput.attributes.name.value];
+  });
 }
 
 function checkLowercase(e) {
-  formInputsArr.forEach((formInput) => {
-    formStorage[formInput.name] = formInput.value;
-  });
-
-  const formStorageStr = JSON.stringify(formStorage);
-  localStorage.setItem('formData', formStorageStr);
-
   if (formInputNodes.value !== formInputNodes.value.toLowerCase()) {
     e.preventDefault();
     errorMessage.textContent = 'Form was not submitted. Email should be lowercased.';
@@ -33,4 +30,20 @@ function checkLowercase(e) {
   }
 }
 
+resetBtn.addEventListener('click', () => {
+  localStorage.clear();
+  Object.keys(formStorage).forEach((key) => {
+    formStorage[key] = '';
+  });
+});
+
+formInputsArr.forEach((formInput) => {
+  formInput.addEventListener('keyup', (e) => {
+    const input = e.target.attributes.name.value;
+    formStorage[input] = e.target.value;
+    const formStorageStr = JSON.stringify(formStorage);
+    localStorage.setItem('formData', formStorageStr);
+  });
+});
 sumbitBtn.addEventListener('click', checkLowercase);
+window.addEventListener('load', loadLocalStorage);
